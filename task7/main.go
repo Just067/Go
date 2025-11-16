c4ccbac8114db890d7e92bbc4b0df65a626c57c7
Малеев Георгий Дмитриевич <george.maleev@gmail.com>
2025-11-16 12:42:45 +0300

    Create main.go

---
+++ task7/main.go
@@ -0,0 +1,26 @@
+package main
+
+import "fmt"
+
+func main() {
+	var score int
+	fmt.Print("Введите число: ")
+	fmt.Scan(&score)
+
+	switch {
+	case score < 0 || score > 100:
+		fmt.Println("invalid")
+	case score == 100:
+		fmt.Println("A")
+	case score >= 90:
+		fmt.Println("A")
+	case score >= 80:
+		fmt.Println("B")
+	case score >= 70:
+		fmt.Println("C")
+	case score >= 60:
+		fmt.Println("D")
+	default:
+		fmt.Println("F")
+	}
+}
